9afecf58c6f36cc3196343e8d4376064f5dd0624
Bunsy <bunsy2014@gmail.com>
2023-11-10 12:09:19 +0700

    fix: fix phone status creation step UI

--- signup-ui/src/templates/ResponsePageTemplate/StatusPageTemplate.tsx
+++ signup-ui/src/templates/ResponsePageTemplate/StatusPageTemplate.tsx
@@ -3,6 +3,7 @@ import { MouseEventHandler } from "react";
 import { ReactComponent as FailedIconSvg } from "~assets/svg/failed-icon.svg";
 import { ReactComponent as SuccessIconSvg } from "~assets/svg/success-icon.svg";
 import { Button } from "~components/ui/button";
+import { Step, StepContent, StepHeader, StepTitle } from "~components/ui/step";
 
 interface StatusPageTemplateProps {
   title: string;
@@ -22,7 +23,34 @@ export const StatusPageTemplate = ({
   handleAction,
 }: StatusPageTemplateProps) => {
   return (
-    <div className="container p-0 max-w-md rounded-[20px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+    <Step>
+      <StepHeader>
+        <StepTitle>
+          {status === "success" ? <SuccessIconSvg /> : <FailedIconSvg />}
+        </StepTitle>
+      </StepHeader>
+      <StepContent>
+        <div className="flex flex-col items-center gap-4 px-4">
+          <h1 className="font-bold text-center text-2xl">{title}</h1>
+          {subtitle && (
+            <h2 className="font-medium text-center text-lg">{subtitle}</h2>
+          )}
+          <p className="text-center text-sm text-gray-500">{description}</p>
+        </div>
+        <Button
+          className="w-full h-16 my-4"
+          variant="secondary"
+          onClick={handleAction}
+        >
+          {action}
+        </Button>
+      </StepContent>
+    </Step>
+  );
+};
+
+{
+  /* <div className="container p-0 max-w-md rounded-[20px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center gap-6 px-4 py-16">
           {status === "success" ? <SuccessIconSvg /> : <FailedIconSvg />}
@@ -33,11 +61,14 @@ export const StatusPageTemplate = ({
             )}
             <p className="text-center text-sm text-gray-500">{description}</p>
           </div>
-          <Button className="w-full h-16" onClick={handleAction}>
+          <Button
+            className="w-full h-16"
+            variant="secondary"
+            onClick={handleAction}
+          >
             {action}
           </Button>
         </div>
       </div>
-    </div>
-  );
-};
+    </div> */
+}
