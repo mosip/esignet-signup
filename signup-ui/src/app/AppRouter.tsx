7fc73a5573d041d7a84e0ba1b553b7e6a9e14ad1
Bunsy <bunsy2014@gmail.com>
2023-11-13 15:27:16 +0700

    feat: navigate to error on http error
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/app/AppRouter.tsx
+++ signup-ui/src/app/AppRouter.tsx
@@ -1,8 +1,9 @@
-import { lazy, ReactNode, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { lazy, ReactNode, Suspense, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import { SIGNUP_ROUTE } from "~constants/routes";
 import { lazyRetry } from "~utils/lazyRetry";
+import { setupResponseInterceptor } from "~services/api.service";
 
 const SignUpPage = lazy(() => lazyRetry(() => import("~pages/SignUpPage")));
 const UnderConstructionPage = lazy(() =>
@@ -19,6 +20,12 @@ const WithSuspense = ({ children }: { children: ReactNode }) => (
 );
 
 export const AppRouter = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setupResponseInterceptor(navigate);
+  }, [navigate]);
+
   return (
     <WithSuspense>
       <Routes>
