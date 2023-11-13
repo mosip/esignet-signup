c0c060a27cdf7a558090308a56a76e8c9d02756d
Bunsy <bunsy2014@gmail.com>
2023-11-13 15:10:25 +0700

    fix: fix layout
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/pages/SignUpPage/SignUpPageLayout.tsx
+++ signup-ui/src/pages/SignUpPage/SignUpPageLayout.tsx
@@ -11,37 +11,33 @@ export const SignUpPageLayout = () => {
 
   if (isLoading || !settings) {
     return (
-      <div className="h-screen flex items-center justify-center">
+      <div className="w-full flex h-[calc(100vh-14vh)] items-center justify-center">
         <Icons.loader2 className="animate-spin" />
       </div>
     );
   }
 
   return (
-    <div className="h-screen">
-      <NavBar />
-      <div className="relative w-full flex h-[calc(100vh-14vh)] items-center justify-center">
-        <div>
-          <img
-            className="absolute left-1 top-1"
-            src="/images/top.png"
-            alt="top left background"
-          />
-        </div>
-        <div className="w-full z-10">
-          <SignUpProvider>
-            <SignUpPage settings={settings} />
-          </SignUpProvider>
-        </div>
-        <div>
-          <img
-            className="absolute bottom-1 right-1"
-            src="/images/bottom.png"
-            alt="bottom right background"
-          />
-        </div>
+    <div className="relative w-full flex h-[calc(100vh-14vh)] items-center justify-center">
+      <div>
+        <img
+          className="absolute left-1 top-1"
+          src="/images/top.png"
+          alt="top left background"
+        />
       </div>
-      <Footer />
+      <div className="w-full z-10">
+        <SignUpProvider>
+          <SignUpPage settings={settings} />
+        </SignUpProvider>
+      </div>
+      <div>
+        <img
+          className="absolute bottom-1 right-1"
+          src="/images/bottom.png"
+          alt="bottom right background"
+        />
+      </div>
     </div>
   );
 };
