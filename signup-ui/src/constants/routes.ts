a173f60b9526a6139d87537977205678c28cfdb1
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-11-13 11:23:44 +0700

    ES-400-sign-up-ui-terms-conditions-privacy-policy-page
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/constants/routes.ts
+++ signup-ui/src/constants/routes.ts
@@ -1,1 +1,4 @@
 export const SIGNUP_ROUTE = "/signup";
+export const ACCOUNT_SETUP_ROUTE = "/account-setup";
+export const TERMS_CONDITIONS_ROUTE = "/terms-and-conditions";
+export const PRIVACY_POLICY_ROUTE = "/privacy-policy";
