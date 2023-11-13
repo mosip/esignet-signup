4b54b31dea655311f3e41e15be99bf4e4c830334
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-11-13 11:23:44 +0700

    ES-400-sign-up-ui-terms-conditions-privacy-policy-page

--- signup-ui/src/app/AppRouter.tsx
+++ signup-ui/src/app/AppRouter.tsx
@@ -1,9 +1,12 @@
 import { lazy, ReactNode, Suspense, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
-import { SIGNUP_ROUTE } from "~constants/routes";
+import { ACCOUNT_SETUP_ROUTE, SIGNUP_ROUTE, TERMS_CONDITIONS_ROUTE, PRIVACY_POLICY_ROUTE } from "~constants/routes";
 import { lazyRetry } from "~utils/lazyRetry";
-import { setupResponseInterceptor } from "~services/api.service";
+import AccountSetupPage from "~pages/AccountSetupPage";
+import TermsAndPrivacyPage from "~pages/TermsAndPrivacyPage";
+import { SignUpProvider } from "~pages/SignUpPage/SignUpProvider";
+import { useTranslation } from "react-i18next";
 
 const SignUpPage = lazy(() => lazyRetry(() => import("~pages/SignUpPage")));
 const UnderConstructionPage = lazy(() =>
@@ -20,16 +23,22 @@ const WithSuspense = ({ children }: { children: ReactNode }) => (
 );
 
 export const AppRouter = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setupResponseInterceptor(navigate);
-  }, [navigate]);
+  const {t} = useTranslation();
 
   return (
     <WithSuspense>
       <Routes>
-        <Route path={SIGNUP_ROUTE} element={<SignUpPage />} />
+        <Route
+          path={SIGNUP_ROUTE}
+          element={
+            <SignUpProvider>
+              <SignUpPage />
+            </SignUpProvider>
+          }
+        />
+        <Route path={ACCOUNT_SETUP_ROUTE} element={<AccountSetupPage />} />
+        <Route path={TERMS_CONDITIONS_ROUTE} element={<TermsAndPrivacyPage title={t("term_title")} content={t("term_content")}/>} />
+        <Route path={PRIVACY_POLICY_ROUTE} element={<TermsAndPrivacyPage title={t("privacy_title")} content={t("privacy_content")}/>} />
         <Route path="*" element={<NotFoundErrorPage />} />
       </Routes>
     </WithSuspense>
