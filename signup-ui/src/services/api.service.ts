bbdcc4008c9ce7e9fd51370ddeb07c6a44384791
Bunsy <bunsy2014@gmail.com>
2023-11-14 14:48:18 +0700

    fix: remove deprecated code
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/services/api.service.ts
+++ signup-ui/src/services/api.service.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosError } from "axios";
-import { StatusCodes } from "http-status-codes";
 import { NavigateFunction } from "react-router-dom";
 
-import { LOCAL_STORAGE_EVENT } from "~constants/localStorage";
 import { SOMETHING_WENT_WRONG } from "~constants/routes";
-import { ApiError } from "~typings/core";
 
 export const API_BASE_URL = process.env.REACT_APP_BASE_URL ?? "/api/";
 
@@ -15,37 +12,6 @@ export class HttpError extends Error {
     this.code = code;
   }
 }
-
-/**
- * Converts possible AxiosError objects to normal Error objects
- *
- * @returns HttpError if AxiosError, else original error
- */
-export const transformAxiosError = (error: Error): ApiError => {
-  if (axios.isAxiosError(error)) {
-    if (error.response) {
-      const statusCode = error.response.status;
-      if (statusCode === StatusCodes.TOO_MANY_REQUESTS) {
-        return new HttpError("Please try again later.", statusCode);
-      }
-      if (typeof error.response.data === "string") {
-        return new HttpError(error.response.data, statusCode);
-      }
-      if (error.response.data?.message) {
-        return new HttpError(error.response.data.message, statusCode);
-      }
-      if (error.response.statusText) {
-        return new HttpError(error.response.statusText, statusCode);
-      }
-      return new HttpError(`Error: ${statusCode}`, statusCode);
-    } else if (error.request) {
-      return new Error(
-        `There was a problem with your internet connection. Please check your network and try again. ${error.message}`
-      );
-    }
-  }
-  return error;
-};
 
 // Create own axios instance with defaults.
 export const ApiService = axios.create({
