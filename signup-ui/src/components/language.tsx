e50aa101915af7d09733efdee379c0f099d5c405
Bunsy <bunsy2014@gmail.com>
2023-11-14 16:09:28 +0700

    feat: add language dropdown and logo
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

---
+++ signup-ui/src/components/language.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+
+import { ReactComponent as TranslationIcon } from "~assets/svg/translation-icon.svg";
+import { langLabel } from "~/i18n/lang";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+export const Language = () => {
+  const { i18n } = useTranslation();
+
+  const handleLanguageChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div className="flex">
+      <TranslationIcon className="w-full h-full" />
+      <Select onValueChange={handleLanguageChange} defaultValue={i18n.language}>
+        <SelectTrigger>
+          <SelectValue
+            placeholder={langLabel[i18n.language as keyof typeof langLabel]}
+          />
+        </SelectTrigger>
+        <SelectContent className="w-12">
+          <SelectItem value="en">English</SelectItem>
+          <SelectItem value="km">ខ្មែរ</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
