c2a229e7bdd9b2ac419fe002dffe540add69b885
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-11-15 15:47:16 +0700

    change term condition page to modal
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/app/AppRouter.tsx
+++ signup-ui/src/app/AppRouter.tsx
@@ -1,11 +1,8 @@
 import { lazy, ReactNode, Suspense, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
-import { ACCOUNT_SETUP_ROUTE, SIGNUP_ROUTE, TERMS_CONDITIONS_ROUTE, PRIVACY_POLICY_ROUTE } from "~constants/routes";
+import { SIGNUP_ROUTE } from "~constants/routes";
 import { lazyRetry } from "~utils/lazyRetry";
-import AccountSetupPage from "~pages/AccountSetupPage";
-import TermsAndPrivacyPage from "~pages/TermsAndPrivacyPage";
-import { SignUpProvider } from "~pages/SignUpPage/SignUpProvider";
 import { useTranslation } from "react-i18next";
 
 const SignUpPage = lazy(() => lazyRetry(() => import("~pages/SignUpPage")));
@@ -28,17 +25,7 @@ export const AppRouter = () => {
   return (
     <WithSuspense>
       <Routes>
-        <Route
-          path={SIGNUP_ROUTE}
-          element={
-            <SignUpProvider>
-              <SignUpPage />
-            </SignUpProvider>
-          }
-        />
-        <Route path={ACCOUNT_SETUP_ROUTE} element={<AccountSetupPage />} />
-        <Route path={TERMS_CONDITIONS_ROUTE} element={<TermsAndPrivacyPage title={t("terms_and_conditions_title")} content={t("terms_and_conditions_content")}/>} />
-        <Route path={PRIVACY_POLICY_ROUTE} element={<TermsAndPrivacyPage title={t("privacy_and_policy_title")} content={t("privacy_and_policy_content")}/>} />
+        <Route path={SIGNUP_ROUTE} element={<SignUpPage />} />
         <Route path="*" element={<NotFoundErrorPage />} />
       </Routes>
     </WithSuspense>
---
+++ signup-ui/src/pages/SignUpPage/AccountSetup/components/TermsAndPrivacyModal.tsx
@@ -0,0 +1,52 @@
+import { useRef, useEffect } from "react";
+import {
+  AlertDialog,
+  AlertDialogContent,
+} from "~components/ui/alert-dialog";
+import { ReactComponent as CloseIconSvg } from "~assets/svg/cross-icon.svg";
+
+interface TermsAndPrivacyModalProps {
+    title: string
+    content: string
+    isOpen: boolean
+    backdrop: string
+    toggleModal: () => void
+  }
+  
+  export const TermsAndPrivacyModal = ({ title, content, isOpen, backdrop, toggleModal }: TermsAndPrivacyModalProps) => {
+    const modalRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      const handleClickOutside = (event: { target: any; }) => {
+        if (backdrop === "static") return;
+        
+        if (isOpen && modalRef.current && !modalRef.current.contains(event.target)) {
+          toggleModal();
+        }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [modalRef, isOpen]);
+
+    return (
+      <AlertDialog open={!!isOpen}>
+        <AlertDialogContent ref={modalRef} className="container max-w-[90%] md:w-[1088px] w-[1088px] p-0 bg-white rounded-lg">
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              {title}
+            </h3>
+            <button type="button" onClick={toggleModal} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+              <CloseIconSvg/>
+            </button>
+          </div>
+          <div className="p-4 md:p-5 space-y-4">
+            <div className="" dangerouslySetInnerHTML={{__html: content}}></div>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
+  };
+  
