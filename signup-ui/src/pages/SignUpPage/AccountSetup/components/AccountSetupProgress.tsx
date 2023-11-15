53768e68716101f756cff6c5c2fbdf2e0598cc9c
Bunsy <bunsy2014@gmail.com>
2023-11-15 16:55:53 +0700

    fix: use svg instead of lucide-react
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/pages/SignUpPage/AccountSetup/components/AccountSetupProgress.tsx
+++ signup-ui/src/pages/SignUpPage/AccountSetup/components/AccountSetupProgress.tsx
@@ -8,7 +8,7 @@ export const AccountSetupProgress = () => {
   return (
     <div className="container p-0 max-w-md rounded-[20px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <div className="flex flex-col items-center gap-6 p-16">
-        <Icons.loader2 className="animate-spin h-20 w-20 text-orange-500" />
+        <Icons.loader className="animate-spin h-20 w-20 text-orange-500" />
         <div>
           <h1 className="font-medium text-center text-lg">
             {t("setup_progress")}
