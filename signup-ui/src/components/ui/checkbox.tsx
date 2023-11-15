0e41bddb9174083bfb3cec529b9a084fffd78fd9
Bunsy <bunsy2014@gmail.com>
2023-11-15 16:55:53 +0700

    fix: use svg instead of lucide-react
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/components/ui/checkbox.tsx
+++ signup-ui/src/components/ui/checkbox.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { Check } from "lucide-react";
 
 import { cn } from "~utils/cn";
+
+import { Icons } from "./icons";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -19,7 +20,7 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-4 w-4" />
+      <Icons.check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
