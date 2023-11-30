e244342fb5ae8f4067470f5119b4c8bddac2da03
rathanak-0080 <148197234+rathanak-0080@users.noreply.github.com>
2023-11-30 17:10:20 +0700

    ES-406 (#20)
    * Sign Up UI: Enabling context based navigation, post successful account creation
    
    * update variable name
    
    * update pom file
    
    ---------
    
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/utils/link.ts
+++ signup-ui/src/utils/link.ts
@@ -1,6 +1,9 @@
+import { Buffer } from "buffer";
+
 export const getSignInRedirectURL = (hash: string): string => {
     if (!!hash) {
-        return process.env.REACT_APP_REDIRECT_SIGN_IN_URL + hash;
+        const signInQueryParams = Buffer.from(hash ?? "", "base64")?.toString();
+        return process.env.REACT_APP_REDIRECT_SIGN_IN_URL + "?" + signInQueryParams;
     }
 
     return process.env.REACT_APP_REDIRECT_SIGN_UP_URL || "/";
