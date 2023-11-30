3be90a92694a9e80b4e50a010a293ecbe2480d40
rathanak-0080 <148197234+rathanak-0080@users.noreply.github.com>
2023-11-30 08:54:03 +0700

    ES-430 (#16)
    * ES-430-signup-backend-api-to-get-the-registration-status
    
    * Add newline
    
    * move to constant
    
    * check case empty transactionId
    
    * Remove idea configure file
    
    ---------
    
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-service/src/main/java/io/mosip/signup/util/SignUpConstants.java
+++ signup-service/src/main/java/io/mosip/signup/util/SignUpConstants.java
@@ -4,4 +4,5 @@ public class SignUpConstants {
     public static final String CHALLENGE_GENERATED = "challenge_generated";
     public static final String CHALLENGE_VERIFIED = "challenge_verified";
     public static final String REGISTERED_CACHE = "registered";
+    public static final String TRANSACTION_ID = "TRANSACTION_ID";
 }
