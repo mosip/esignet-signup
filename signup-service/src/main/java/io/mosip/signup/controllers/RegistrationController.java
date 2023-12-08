83fc8753aabe360ee52ec19847e0018a8cc5c820
Anusha Sunkada <sunkadaeanusha@gmail.com>
2023-12-08 13:23:34 +0530

    ES-391 (#35)
    * ES-391 Added fixes
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * Added CSRF support
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * ES-391
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    ---------
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/controllers/RegistrationController.java
+++ signup-service/src/main/java/io/mosip/signup/controllers/RegistrationController.java
@@ -12,14 +12,19 @@ import io.mosip.signup.dto.VerifyChallengeRequest;
 import io.mosip.signup.dto.VerifyChallengeResponse;
 import io.mosip.signup.exception.SignUpException;
 import io.mosip.signup.services.RegistrationService;
+import io.mosip.signup.util.ErrorConstants;
 import io.mosip.signup.util.SignUpConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import static io.mosip.signup.util.SignUpConstants.EMTPY;
 
 @Slf4j
 @RestController
@@ -31,7 +36,7 @@ public class RegistrationController {
 
     @PostMapping("/generate-challenge")
     public ResponseWrapper<GenerateChallengeResponse> generateChallenge (@Valid @RequestBody RequestWrapper<GenerateChallengeRequest> requestWrapper,
-                                                                         @CookieValue(name = SignUpConstants.TRANSACTION_ID, defaultValue = "") String transactionId)
+                                                                         @CookieValue(name = SignUpConstants.TRANSACTION_ID, defaultValue = EMTPY) String transactionId)
             throws SignUpException {
         ResponseWrapper<GenerateChallengeResponse> responseWrapper = new ResponseWrapper<>();
         responseWrapper.setResponse(registrationService.generateChallenge(requestWrapper.getRequest(), transactionId));
@@ -41,7 +46,8 @@ public class RegistrationController {
 
     @PostMapping("/verify-challenge")
     public ResponseWrapper<VerifyChallengeResponse> verifyChallenge(@Valid @RequestBody RequestWrapper<VerifyChallengeRequest> requestWrapper,
-                                                                    @CookieValue(SignUpConstants.TRANSACTION_ID) String transactionId)
+                                                                   @Valid @NotBlank(message = ErrorConstants.INVALID_TRANSACTION)
+                                                                   @CookieValue(value = SignUpConstants.TRANSACTION_ID, defaultValue = EMTPY) String transactionId)
             throws SignUpException {
         ResponseWrapper<VerifyChallengeResponse> responseWrapper = new ResponseWrapper<>();
         responseWrapper.setResponseTime(IdentityProviderUtil.getUTCDateTime());
@@ -51,7 +57,8 @@ public class RegistrationController {
 
     @PostMapping("/register")
     public ResponseWrapper<RegisterResponse> register(@Valid @RequestBody RequestWrapper<RegisterRequest> requestWrapper,
-                                                      @CookieValue(SignUpConstants.TRANSACTION_ID) String transactionId)
+                                                      @Valid @NotBlank(message = ErrorConstants.INVALID_TRANSACTION)
+                                                      @CookieValue(value = SignUpConstants.VERIFIED_TRANSACTION_ID, defaultValue = EMTPY) String transactionId)
             throws SignUpException {
         ResponseWrapper<RegisterResponse> responseWrapper = new ResponseWrapper<>();
         responseWrapper.setResponse(registrationService.register(requestWrapper.getRequest(), transactionId));
@@ -61,7 +68,8 @@ public class RegistrationController {
 
     @GetMapping("/status")
     public ResponseWrapper<RegistrationStatusResponse> getRegistrationStatus(
-            @CookieValue(SignUpConstants.TRANSACTION_ID) String transactionId) {
+            @Valid @NotBlank(message = ErrorConstants.INVALID_TRANSACTION)
+            @CookieValue(value = SignUpConstants.VERIFIED_TRANSACTION_ID, defaultValue = EMTPY) String transactionId) {
         ResponseWrapper<RegistrationStatusResponse> responseWrapper = new ResponseWrapper<RegistrationStatusResponse>();
         responseWrapper.setResponse(registrationService.getRegistrationStatus(transactionId));
         responseWrapper.setResponseTime(IdentityProviderUtil.getUTCDateTime());
