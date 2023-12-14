e5cbde025b7fe7a69eba331baa1c36358fb59a4b
Anusha Sunkada <sunkadaeanusha@gmail.com>
2023-12-14 14:01:53 +0530

    ES-430 (#42)
    * ES-430
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * ES-430
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * Updated template messages
    
    ---------
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/util/RegistrationStatus.java
+++ signup-service/src/main/java/io/mosip/signup/util/RegistrationStatus.java
@@ -1,7 +1,14 @@
 package io.mosip.signup.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum RegistrationStatus {
     PENDING,
     COMPLETED,
-    FAILED
+    FAILED;
+
+    public static List<RegistrationStatus> getEndStatuses() {
+        return Arrays.asList(RegistrationStatus.COMPLETED, RegistrationStatus.FAILED);
+    }
 }
