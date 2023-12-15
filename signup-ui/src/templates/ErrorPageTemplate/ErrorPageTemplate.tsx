db81423745d9dab1c6b4e509c2476f4d1b4a88ec
lengheang-0116 <150209836+lengheang-0116@users.noreply.github.com>
2023-12-15 10:46:49 +0700

    ES-405 - Mobile responsive on verify OTP pages (#40)
    * Mobile responsive on verify OTP pages
    
    * updated margin top of setup account
    
    * remove
    
    * removed
    
    * removed
    
    * rolback java spring properties file
    
    * rollback java appilcation properties
    
    * rollback java appilcation properties
    
    * updated and moved input textbox size to appCSS file
    
    * Updated: resolved comment by member
    
    * rollback css class
    
    * fixed breaking layout on Nest Hub device
    
    * spacing'
    
    * updated: Remove extra spaces and duplicated characters
    
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/templates/ErrorPageTemplate/ErrorPageTemplate.tsx
+++ signup-ui/src/templates/ErrorPageTemplate/ErrorPageTemplate.tsx
@@ -10,11 +10,11 @@ export const ErrorPageTemplate = ({
   image,
 }: ErrorPageTemplateProps) => {
   return (
-    <div className="w-full flex h-[calc(100vh-14vh)] items-center justify-center p-16 px-32">
-      <div className="w-full h-full bg-white rounded-[10px] flex flex-col items-center justify-center gap-y-12 shadow-[0_4px_10px_rgb(0,0,0,0.1)]">
+    <div className="flex h-[calc(100vh-14vh)] w-full items-center justify-center p-16 px-32 sm:px-[30px]">
+      <div className="h-full bg-white flex w-full flex-col items-center justify-center gap-y-12 rounded-xl shadow-lg sm:shadow-none">
         {image}
         <div className="flex flex-col items-center gap-y-2">
-          <h1 className="text-2xl">{title}</h1>
+          <h1 className="text-center text-2xl">{title}</h1>
           <p className="text-center text-gray-500">{description}</p>
         </div>
       </div>
