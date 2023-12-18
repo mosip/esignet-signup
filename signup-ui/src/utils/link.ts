3312e77dea91ee7cb09f3115d13ee1765542de99
rathanak-0080 <148197234+rathanak-0080@users.noreply.github.com>
2023-12-18 17:05:17 +0700

    Add translation keys from ID repo (#48)
    * Add translation keys from ID repo
    
    * Add khmer translation
    
    * Add default path
    
    ---------
    
    Co-authored-by: Sreang Rathanak <sreangrathanak@yahoo.com>
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/utils/link.ts
+++ signup-ui/src/utils/link.ts
@@ -1,10 +1,10 @@
 import { Buffer } from "buffer";
 
-export const getSignInRedirectURL = (redirectUrl: string | undefined, hash: string): string => {
+export const getSignInRedirectURL = (redirectUrl: string | undefined, hash: string, defaultPath: string): string => {
   if (!!hash) {
     const signInQueryParams = Buffer.from(hash ?? "", "base64")?.toString();
     return redirectUrl + "?" + signInQueryParams;
   }
 
-  return "/";
+  return defaultPath;
 };
