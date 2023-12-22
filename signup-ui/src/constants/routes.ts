3a432ade7bef81e2aefa6c04fd8ce6c84d09d0c7
rathanak-0080 <148197234+rathanak-0080@users.noreply.github.com>
2023-12-22 14:40:12 +0700

    ES-613 (#55)
    * UI: Signup, forgot password landing page
    
    * add const for redirect route
    
    ---------
    
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/constants/routes.ts
+++ signup-ui/src/constants/routes.ts
@@ -1,1 +1,5 @@
+export const ROOT_ROUTE = "/";
 export const SIGNUP_ROUTE = "/signup";
+export const RESET_PASSWORD = "/reset-password";
+export const SOMETHING_WENT_WRONG = "/something-went-wrong";
+export const UNDER_CONSTRUCTION = "/under-construction";
