4dd893e963520f3a764bf2a965056ab04d1ef683
panharith-0118 <148301211+panharith-0118@users.noreply.github.com>
2023-12-27 20:13:54 +0700

    ES-604 (#53)
    Signed-off-by: pr <panharith-an@mptc.gov.kh>

--- signup-service/src/main/java/io/mosip/signup/dto/UserInfoMap.java
+++ signup-service/src/main/java/io/mosip/signup/dto/UserInfoMap.java
@@ -1,13 +1,12 @@
 package io.mosip.signup.dto;
 
 import io.mosip.signup.util.ErrorConstants;
+import io.mosip.signup.validator.LanguageValue;
 import io.mosip.signup.validator.PhoneNumber;
 import io.mosip.signup.validator.Language;
 import lombok.Data;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
@@ -17,7 +16,8 @@ public class UserInfoMap {
     private String phone;
 
     @NotEmpty(message = ErrorConstants.INVALID_FULLNAME)
-    private List<@Valid LanguageTaggedValue> fullName;
+    private List<@LanguageValue(valuePatternKey = "mosip.signup.fullname.pattern")
+            LanguageTaggedValue> fullName;
 
     @Language
     private String preferredLang;
---
+++ signup-service/src/main/java/io/mosip/signup/validator/LanguageValueValidator.java
@@ -0,0 +1,30 @@
+package io.mosip.signup.validator;
+
+import io.mosip.signup.dto.LanguageTaggedValue;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class LanguageValueValidator implements ConstraintValidator<LanguageValue, LanguageTaggedValue> {
+
+    private String valuePattern;
+    private String language;
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    public void initialize(LanguageValue value) {
+        this.valuePattern = environment.getProperty(value.valuePatternKey());
+        this.language = value.language();
+    }
+
+    @Override
+    public boolean isValid(LanguageTaggedValue value, ConstraintValidatorContext constraintValidatorContext) {
+        if(value == null)
+            return false;
+        return value.getLanguage().equals(this.language) && value.getValue().matches(valuePattern);
+    }
+}
