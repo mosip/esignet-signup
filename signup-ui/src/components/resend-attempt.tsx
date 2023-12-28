db79d9ce8757a3eace921090293ea09153f5b2c5
bunsy-0900 <148200748+bunsy-0900@users.noreply.github.com>
2023-12-28 09:26:02 +0700

    ES-560 - UI changes in verify_challenge input in "Enter OTP page" (#58)
    * feat: implement forgot password landing page
    
    * fix: remove popover for fullname
    
    * fix: fix fullname in specific lng validation
    
    * fix: fix phone number validation
    
    * fix: disable reload on language change
    
    * feat: implement otp page for reset password
    
    * add Khmer translation for signup (#46)
    
    * add Khmer translation for signup
    
    * Update translation
    
    ---------
    
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    
    * fix: fix otp attempt left message
    
    * fix: fix regenerate challenge
    
    * fix: fix verify otp
    
    ---------
    
    Co-authored-by: Bunsy <bunsy2014@gmail.com>
    Co-authored-by: rathanak-0080 <148197234+rathanak-0080@users.noreply.github.com>
    Co-authored-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/components/resend-attempt.tsx
+++ signup-ui/src/components/resend-attempt.tsx
@@ -3,20 +3,23 @@ import { useTranslation } from "react-i18next";
 interface ResendAttemptProps {
   currentAttempts: number;
   totalAttempts: number;
+  attemptRetryAfter?: number;
 }
 
 export const ResendAttempt = ({
   currentAttempts,
   totalAttempts,
+  attemptRetryAfter = 5,
 }: ResendAttemptProps) => {
   const { t } = useTranslation();
   return (
     <>
       {currentAttempts < totalAttempts && (
-        <div className="w-full bg-[#FFF7E5] rounded-md p-2 text-[#8B6105] text-center text-sm font-semibold">
+        <div className="w-max rounded-md bg-[#FFF7E5] p-2 px-8 text-center text-sm font-semibold text-[#8B6105]">
           {t("attempts_left", {
             attemptLeft: currentAttempts,
             totalAttempt: totalAttempts,
+            attemptRetryAfter: attemptRetryAfter,
           })}
         </div>
       )}
