1d1432bda0cba80b2440060ffe88fbb0480cebe8
lengheang-0116 <150209836+lengheang-0116@users.noreply.github.com>
2023-12-28 14:55:16 +0700

    ES-598 - UI Forgot Password Mobile responsiveness (#63)
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/layouts/PageLayout.tsx
+++ signup-ui/src/layouts/PageLayout.tsx
@@ -4,15 +4,15 @@ interface PageLayoutProps {
 
 export const PageLayout = ({ children }: PageLayoutProps) => {
   return (
-    <div className="relative flex flex-1 items-center justify-center">
+    <div className="relative flex flex-1 items-center justify-center sm:flex-none">
       <img
-        className="absolute left-1 top-1"
+        className="absolute left-1 top-1 block sm:hidden"
         src="/images/top.png"
         alt="top left background"
       />
       <div className="z-10 w-full">{children}</div>
       <img
-        className="absolute bottom-1 right-1"
+        className="absolute bottom-1 right-1 block sm:hidden"
         src="/images/bottom.png"
         alt="bottom right background"
       />
