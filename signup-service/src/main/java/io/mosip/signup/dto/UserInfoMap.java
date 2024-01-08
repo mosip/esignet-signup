c85fd5a2d094c72df8d69bf2fc8fc81a10c13867
Anusha Sunkada <sunkadaeanusha@gmail.com>
2024-01-08 20:06:57 +0530

    ES-391 fixed validation issue (#84)
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-service/src/main/java/io/mosip/signup/dto/UserInfoMap.java
+++ signup-service/src/main/java/io/mosip/signup/dto/UserInfoMap.java
@@ -6,6 +6,7 @@ import io.mosip.signup.validator.PhoneNumber;
 import io.mosip.signup.validator.Language;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class UserInfoMap {
     private String phone;
 
     @NotEmpty(message = ErrorConstants.INVALID_FULLNAME)
-    private List<@LanguageValue(valuePatternKey = "mosip.signup.fullname.pattern")
+    private List<@Valid @LanguageValue(valuePatternKey = "mosip.signup.fullname.pattern")
             LanguageTaggedValue> fullName;
 
     @Language
