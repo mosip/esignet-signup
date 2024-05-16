e2a642fc8cea34b5c707203ff50a0002afc3e376
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-05-16 11:44:51 +0700

    fix: remove deprecated comments
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/constants/browsers.ts
+++ signup-ui/src/constants/browsers.ts
@@ -1,7 +1,5 @@
 export const BROWSER_MIN_COMPATIBILITY: { [key: string]: string } = {
-  // TODO: revert back to 118.0.5993.72
   chrome: "118.0.5993.72",
-  // chrome: "130.0.5993.72",
   firefox: "118.0.2",
   edge: "118.0.2088.46",
   safari: "14.1",
