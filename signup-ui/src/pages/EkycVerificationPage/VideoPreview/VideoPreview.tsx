c67284cac18b8fe47dcd6d188b0d979e4dc1a679
Gk <76690271+GurukiranP@users.noreply.github.com>
2024-05-16 12:19:57 +0530

    [ES-1115]: ekyc steps UI screen. (#156)
    * [ES-1115]: ekyc steps screen UI.
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    
    * [ES-1115]: ekyc steps screen UI.
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    
    ---------
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- signup-ui/src/pages/EkycVerificationPage/VideoPreview/VideoPreview.tsx
+++ signup-ui/src/pages/EkycVerificationPage/VideoPreview/VideoPreview.tsx
@@ -173,7 +173,7 @@ export const VideoPreview = () => {
               <Button
                 id="cancel-preview-button"
                 name="cancel-preview-button"
-                variant="outline"
+                variant="cancel_outline"
                 className="w-full p-4 font-semibold"
                 onClick={handleCancel}
               >
