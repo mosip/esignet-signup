a6008b8dcfda7c98b6d247878ff8af94d3ce408d
Gk <76690271+GurukiranP@users.noreply.github.com>
2024-05-28 16:12:10 +0530

    [ES-1115] Minor css and functionality fix. (#172)
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- signup-ui/src/components/ui/nav-bar.tsx
+++ signup-ui/src/components/ui/nav-bar.tsx
@@ -5,7 +5,7 @@ import { Language } from "~components/language";
 const NavBar = () => {
   return (
     <nav className="sticky top-0 z-40 h-[70px] w-full border-gray-500 bg-white px-2 py-2 shadow-md sm:px-0">
-      <div className="container flex h-full items-center justify-between">
+      <div className="container flex h-full items-center justify-between md:px-[0.5rem] py-2 px-[4rem]">
         <div className="ltr:ml-1 ltr:sm:ml-8 rtl:mr-1 rtl:sm:mr-8">
           <img className="brand-logo" alt="brand_logo" />
         </div>
