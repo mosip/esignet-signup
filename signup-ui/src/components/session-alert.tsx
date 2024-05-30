62311c1ee2310619ee7f1d8ebbb15a9f2bbc9a0a
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-05-30 15:23:10 +0700

    wip: add session popup message
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

---
+++ signup-ui/src/components/session-alert.tsx
@@ -0,0 +1,122 @@
+import { useCallback, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useIdleTimer } from "react-idle-timer";
+import { useLocation } from "react-router-dom";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~components/ui/alert-dialog";
+import { Icons } from "~components/ui/icons";
+import { getSignInRedirectURL } from "~utils/link";
+import { convertTime } from "~utils/timer";
+import { useSettings } from "~pages/shared/queries";
+
+// TODO: replace with the timeout and promptBeforeIdle from API
+const timeout = 15_000;
+const promptBeforeIdle = 12_000;
+const ON_ACTION_THROTTLE = 500;
+
+export const SessionAlert = () => {
+  const { t } = useTranslation();
+
+  const { data: settings } = useSettings();
+  const { hash: fromSignInHash } = useLocation();
+
+  const [sessionRemainingTimeout, setSessionRemainingTimeout] =
+    useState<number>(timeout);
+  const [openSessionAlert, setOpenSessionAlert] = useState<boolean>(false);
+
+  const handleOnActive = useCallback(() => {
+    setOpenSessionAlert(false);
+  }, []);
+
+  const handleOnPrompt = useCallback(() => {
+    setOpenSessionAlert(true);
+  }, []);
+
+  const handleOnIdle = useCallback(() => {
+    setOpenSessionAlert(true);
+  }, []);
+
+  const { getRemainingTime, activate, isIdle } = useIdleTimer({
+    onActive: handleOnActive,
+    onIdle: handleOnIdle,
+    onPrompt: handleOnPrompt,
+    timeout,
+    promptBeforeIdle,
+    throttle: ON_ACTION_THROTTLE,
+    stopOnIdle: true,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSessionRemainingTimeout(Math.ceil(getRemainingTime() / 1000));
+    }, ON_ACTION_THROTTLE);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [getRemainingTime]);
+
+  const handleContinueSession = useCallback(() => {
+    activate();
+    handleOnActive();
+  }, []);
+
+  const handleReturnToLogin = (e: any) => {
+    e.preventDefault();
+    window.location.href = getSignInRedirectURL(
+      settings?.response.configs["signin.redirect-url"],
+      fromSignInHash,
+      "/signup"
+    );
+  };
+
+  return (
+    <AlertDialog open={openSessionAlert}>
+      <AlertDialogContent className="rounded-2xl ring-0">
+        <AlertDialogHeader className="m-2">
+          <AlertDialogTitle className="flex flex-col items-center justify-center gap-y-4 text-2xl">
+            <>
+              <Icons.failed />
+              {isIdle() ? t("session.expired.title") : t("session.alert.title")}
+            </>
+          </AlertDialogTitle>
+          <div className="text-center text-muted-dark-gray">
+            {isIdle() ? (
+              t("session.expired.description")
+            ) : (
+              <>
+                <p>{t("session.alert.description")}</p>
+                <p className="mt-2 inline-block rounded-md bg-alert p-2 px-8 text-center text-sm font-semibold text-primary">
+                  {t("session.alert.countDown", {
+                    remainingTimeBeforePrompt: convertTime(
+                      sessionRemainingTimeout
+                    ),
+                  })}
+                </p>
+              </>
+            )}
+          </div>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            id="cs-button"
+            name="cs-button"
+            onClick={isIdle() ? handleReturnToLogin : handleContinueSession}
+            className="w-full bg-primary"
+          >
+            {isIdle()
+              ? t("session.expired.returnToLogin")
+              : t("session.alert.continueSession")}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- signup-ui/src/layouts/AppLayout.tsx
+++ signup-ui/src/layouts/AppLayout.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+// import { useSessionTimerContext } from "~contexts/SessionTimerContext";
 import Footer from "~components/ui/footer";
 import NavBar from "~components/ui/nav-bar";
+import { SessionAlert } from "~components/session-alert";
 
 export const AppLayout = () => {
+  // const {
+  //   showWarning,
+  //   showSessionTimeout,
+  //   sessionDurationLeft,
+  //   isWarningTimerRunning,
+  // } = useSessionTimerContext();
+
+
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar />
       <div className="relative flex flex-grow flex-col sm:bg-white">
+        {/* {(showWarning || showSessionTimeout) && <SessionAlert /> } */}
+        {<SessionAlert />}
         <Outlet />
         <Footer />
       </div>
