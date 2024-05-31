b9f3cada2d4fa4b6cc966b71a49b72a05a7c172d
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2024-05-31 17:02:21 +0530

    [MODIFIED] change working flow for lang config
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- signup-ui/public/lang-config.js
+++ signup-ui/public/lang-config.js
@@ -4,11 +4,11 @@ const hashCode = window.location.hash.substring(1);
 // Decode the string
 const decodedBase64 = atob(hashCode);
 
+// converting the decoded string to URLSearchParams
 var urlSearchParams = new URLSearchParams(decodedBase64);
 
-// Convert the decoded string to JSON
-var jsonObject = {};
-urlSearchParams.forEach(function (value, key) {
-  jsonObject[key] = value;
-});
-
+// Check if the URLSearchParams has the key 'ui_locales'
+// If it has, set the value of 'ui_locales' to the DEFAULT_LANG
+if (urlSearchParams.has('ui_locales')) {
+  window._env_.DEFAULT_LANG = urlSearchParams.get('ui_locales');
+}
