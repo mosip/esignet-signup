aa5e8c60f3846162546efb83864ee35aa6708b49
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2024-05-31 20:09:38 +0530

    [FIXES] ui_locales issue in prod
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- signup-ui/public/lang-config.js
+++ signup-ui/public/lang-config.js
@@ -10,5 +10,5 @@ var urlSearchParams = new URLSearchParams(decodedBase64);
 // Check if the URLSearchParams has the key 'ui_locales'
 // If it has, set the value of 'ui_locales' to the DEFAULT_LANG
 if (urlSearchParams.has('ui_locales')) {
-  window._env_.DEFAULT_LANG = urlSearchParams.get('ui_locales');
+  window._env_.DEFAULT_LANG = urlSearchParams.get('ui_locales').split('-')[0];
 }
