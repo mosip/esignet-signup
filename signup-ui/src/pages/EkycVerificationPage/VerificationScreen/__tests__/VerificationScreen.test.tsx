b4cb25bdd70848ad67768f4f5d71c33926168aa7
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-07-02 15:38:05 +0700

    test: initi verification screen test cases
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

---
+++ signup-ui/src/pages/EkycVerificationPage/VerificationScreen/__tests__/VerificationScreen.test.tsx
@@ -0,0 +1,36 @@
+describe("Web socket connection between the front end and back end", () => {
+  it("should be on", () => {});
+});
+
+describe("VerificationScreen", () => {
+  it("should show onscreen instructions above the video frame sent from eKYC provider", () => {});
+
+  it("should show solid colors across the full screen for color based frame verification", () => {});
+
+  it("should show liveliness verification screen", () => {});
+
+  it("should show NID verification screen", () => {});
+
+  it("should show feedback message when verification fails", () => {});
+
+  it("should show warning message if there is any technical issue", () => {});
+
+  it("should be redirected to the leading screen when the verification is successful", () => {});
+});
+
+/**
+ *
+Video ekyc screen should show onscreen instructions (Above the video frame) sent from eKYC provider.
+
+Video ekyc screen should show solid colors across the full screen for color based frame verification.
+
+Video ekyc screen should show liveliness verification screen.
+
+Video ekyc screen should show NID verification screen.
+
+Video ekyc screen should show feedback message when verification fails.
+
+Video ekyc screen should show warning message if there is any technical issue.
+
+Video ekyc screen should be redirected to the leading screen when the verification is successful.
+*/
