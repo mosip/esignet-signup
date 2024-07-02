edd0bd8c9786df6809a762499caccedf1fe9610a
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-07-02 15:54:51 +0700

    fix: init verification test cases
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/pages/EkycVerificationPage/VerificationScreen/__tests__/VerificationScreen.test.tsx
+++ signup-ui/src/pages/EkycVerificationPage/VerificationScreen/__tests__/VerificationScreen.test.tsx
@@ -1,19 +1,89 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+import { renderWithClient } from "~utils/test";
+
+import { VerificationScreen } from "../VerificationScreen";
+
 describe("Web socket connection between the front end and back end", () => {
-  it("should be on", () => {});
+  const queryCache = new QueryCache();
+  const queryClient = new QueryClient({ queryCache });
+
+  it("should be on", () => {
+    // will add the test implementation once some web socket structure is given
+
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
 });
 
 describe("VerificationScreen", () => {
-  it("should show onscreen instructions above the video frame sent from eKYC provider", () => {});
+  const queryCache = new QueryCache();
+  const queryClient = new QueryClient({ queryCache });
 
-  it("should show solid colors across the full screen for color based frame verification", () => {});
+  it("should show onscreen instructions above the video frame sent from eKYC provider", () => {
+    // Arrange
 
-  it("should show liveliness verification screen", () => {});
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
 
-  it("should show NID verification screen", () => {});
+    // Assert
+  });
 
-  it("should show feedback message when verification fails", () => {});
+  it("should show solid colors across the full screen for color based frame verification", () => {
+    // Arrange
 
-  it("should show warning message if there is any technical issue", () => {});
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
 
-  it("should be redirected to the leading screen when the verification is successful", () => {});
+    // Assert
+  });
+
+  it("should show liveliness verification screen", () => {
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
+
+  it("should show NID verification screen", () => {
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
+
+  it("should show feedback message when verification fails", () => {
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
+
+  it("should show warning message if there is any technical issue", () => {
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
+
+  it("should be redirected to the leading screen when the verification is successful", () => {
+    // Arrange
+
+    // Act
+    renderWithClient(queryClient, <VerificationScreen />);
+
+    // Assert
+  });
 });
