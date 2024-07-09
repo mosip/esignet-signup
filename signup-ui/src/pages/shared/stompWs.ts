13415ee7e51001d52bdc62dcdadb11e2c136a707
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2024-07-09 19:08:37 +0530

    [MODIFIED] verification screen
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- signup-ui/src/pages/shared/stompWs.ts
+++ signup-ui/src/pages/shared/stompWs.ts
@@ -4,6 +4,7 @@ import { Client } from "@stomp/stompjs";
 const useStompClient = (url: string, options = {}) => {
   const [client, setClient] = useState<any>(null);
   const [connected, setConnected] = useState<boolean>(false);
+  const [subscription, setSubscription] = useState<any>(null);
 
   useEffect(() => {
     const stompClient = new Client({
@@ -22,9 +23,9 @@ const useStompClient = (url: string, options = {}) => {
     };
   }, [url]);
 
-  const subscribe = (destination: string, callback: any) => {
+  const subscribe = (destination: string, callback: any): any => {
     if (connected) {
-      client?.subscribe(destination, callback);
+      setSubscription(client?.subscribe(destination, callback));
     } else {
       console.warn("Client not connected yet!");
     }
@@ -38,7 +39,15 @@ const useStompClient = (url: string, options = {}) => {
     }
   };
 
-  return { client, connected, subscribe, publish };
+  const unsubscribe = () => {
+    if (subscription) {
+      subscription.unsubscribe();
+    } else {
+      console.warn("No subscription to unsubscribe from!");
+    }
+  }
+
+  return { client, connected, subscribe, publish, unsubscribe };
 };
 
 export default useStompClient;
