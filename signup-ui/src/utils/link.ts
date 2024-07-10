5f7983d5d69b4f788aed78e2b9afbaa2b0a70ea6
Sreang Rathanak <sreangrathanajk@yahoo.com>
2024-07-10 11:52:35 +0700

    User preferred selected language is not retained during redirection or when user navigates back post perform action suggested on the popup
    Signed-off-by: Sreang Rathanak <sreangrathanajk@yahoo.com>

--- signup-ui/src/utils/link.ts
+++ signup-ui/src/utils/link.ts
@@ -1,10 +1,39 @@
-import { Buffer } from "buffer";
-
-export const getSignInRedirectURL = (redirectUrl: string | undefined, hash: string, defaultPath: string): string => {
+export const getSignInRedirectURL = (
+  redirectUrl: string | undefined,
+  hash: string,
+  defaultPath: string
+): string => {
+  const locale = localStorage.getItem("esignet-signup-language");
   if (!!hash) {
-    const signInQueryParams = Buffer.from(hash ?? "", "base64")?.toString();
-    return redirectUrl + "?" + signInQueryParams;
+    const signInQueryParams = replaceUILocales(hash, locale);
+    return redirectUrl + "?" + signInQueryParams.toString();
   }
+  return defaultPath + "?ui_locales=" + locale;
+};
 
-  return defaultPath;
+export const replaceUILocales = (
+  hash: string,
+  locale: string | null
+): URLSearchParams => {
+  // Convert the decoded string to JSON
+  const decodedBase64 = atob(hash.substring(1));
+
+  var urlSearchParams = new URLSearchParams(decodedBase64);
+
+  // Convert the decoded string to JSON
+  var jsonObject: Record<string, string> = {};
+  urlSearchParams.forEach(function (value, key) {
+    jsonObject[key] = value;
+    // Assign the current i18n language to the ui_locales
+    if (key === "ui_locales") {
+      jsonObject[key] = locale || value;
+    }
+  });
+
+  // Convert the JSON back to decoded string
+  Object.entries(jsonObject).forEach(([key, value]) => {
+    urlSearchParams.set(key, value);
+  });
+
+  return urlSearchParams;
 };
