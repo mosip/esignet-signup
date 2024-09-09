d0b8645c9b73e6696fe5cc8d3fa597d9caea9e81
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-09-09 15:57:56 +0700

    fix: fix browser compatibility issue
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/pages/EkycVerificationPage/VerificationSteps/utils/checkBrowserCompatible.ts
+++ signup-ui/src/pages/EkycVerificationPage/VerificationSteps/utils/checkBrowserCompatible.ts
@@ -1,35 +1,31 @@
+import Bowser, { BROWSER_MAP } from "bowser";
+import { findKey } from "lodash";
+
 import { BROWSER_MIN_COMPATIBILITY } from "~constants/browsers";
 import { compareVersions } from "~utils/browser";
 
-/**
- * Checks if the browser is compatible based on its version.
- *
- * This function gets the user agent string and defines a regular expression for each browser to extract its version.
- * It then loops over the regular expressions, and if it finds a match in the user agent string, it compares the extracted version with the minimum compatible version for that browser.
- * If the browser version is greater than or equal to the minimum compatible version, it returns true.
- * If no match is found for any browser, it returns false.
- * @param {Object} minBrowserCompatibility - An object containing the minimum compatible version for each browser. The default value is BROWSER_MIN_COMPATIBILITY.
- * @returns {boolean} - True if the browser is compatible, false otherwise.
- */
 export const checkBrowserCompatible = (
   minBrowserCompatibility: { [key: string]: string } = BROWSER_MIN_COMPATIBILITY
 ) => {
-  const userAgent = navigator.userAgent;
-  const browserVersionRegex = {
-    edge: /Edg\/(\d+\.\d+\.\d+\.\d+)/,
-    safari: /Version\/(\d+\.\d+)/,
-    firefox: /Firefox\/(\d+\.\d+)/,
-    chrome: /Chrome\/(\d+\.\d+\.\d+\.\d+)/,
-  };
+  const browserInfo = Bowser.parse(window.navigator.userAgent);
 
-  for (const [browser, regex] of Object.entries(browserVersionRegex)) {
-    const browserVersionMatch = userAgent.match(regex);
-    if (browserVersionMatch) {
-      return compareVersions(
-        browserVersionMatch[1],
-        minBrowserCompatibility[browser]
-      );
-    }
+  const browserName = findKey(
+    BROWSER_MAP,
+    (b) => b === browserInfo.browser.name
+  );
+
+  if (!browserName) return false;
+
+  const minBrowserVersion = minBrowserCompatibility[browserName];
+  const currentBrowserVersion = browserInfo.browser.version;
+
+  if (minBrowserVersion === "" || !currentBrowserVersion) return false;
+
+  const isEligible = compareVersions(currentBrowserVersion, minBrowserVersion);
+
+  if (isEligible) {
+    return true;
   }
+
   return false;
 };
