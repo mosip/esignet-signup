a9f5820533aada25e8051cc9831a189847233aae
bunsy-0900 <148200748+bunsy-0900@users.noreply.github.com>
2024-09-12 16:16:36 +0700

    fix: fix grid items do not have the same height (#349)
    Signed-off-by: bunsy-0900 <bunsy-chhay@mptc.gov.kh>

--- signup-ui/src/pages/EkycVerificationPage/KycProviderList/components/KycProviderCardLayout.tsx
+++ signup-ui/src/pages/EkycVerificationPage/KycProviderList/components/KycProviderCardLayout.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import { Icons } from "~components/ui/icons";
 import { cn } from "~utils/cn";
 
 interface KycProviderCardLayoutProps {
@@ -13,6 +14,7 @@ interface KycProviderCardLayoutProps {
   resumeOnSuccess: boolean;
   selected: boolean;
   langMap: { [key: string]: string };
+  onKycProvidersSelection: () => void;
 }
 
 export const KycProviderCardLayout = ({
@@ -24,59 +26,48 @@ export const KycProviderCardLayout = ({
   retryOnFailure,
   resumeOnSuccess,
   selected,
-  langMap
+  langMap,
+  onKycProvidersSelection,
 }: KycProviderCardLayoutProps) => {
   const { i18n } = useTranslation();
 
-  const [providerName, setProviderName] = useState(displayName[langMap[i18n.language]]);
+  const [providerName, setProviderName] = useState(
+    displayName[langMap[i18n.language]]
+  );
 
   useEffect(() => {}, [providerName]);
 
   useEffect(() => {
-    if(langMap) {
+    if (langMap) {
       setProviderName(displayName[langMap[i18n.language]]);
     }
   }, [langMap, i18n.language]);
 
   return (
     <>
-      <div id={id}>
-        <div
-          className={cn(
-            "container max-w-lg cursor-pointer rounded-lg bg-white p-4 sm:max-w-none sm:rounded-lg",
-            selected ? "check-box kyc-box-selected" : "kyc-box"
+      <div
+        id={id}
+        className={cn(
+          "col-span-1 flex cursor-pointer flex-col rounded-lg bg-white p-4",
+          selected ? "check-box kyc-box-selected" : "kyc-box"
+        )}
+        onClick={onKycProvidersSelection}
+      >
+        <>
+          {selected && (
+            <div className="check_box">
+              <Icons.check className="text-white" />
+            </div>
           )}
-        >
-          <>
-            {selected && (
-              <div className="check_box">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="28"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="lucide lucide-check"
-                >
-                  <path d="M20 6 9 17l-5-5" />
-                </svg>
-              </div>
-            )}
-            <div className="flex flex-col justify-start">
-            <div className="w-[52px] kyc-box-icon rounded-md bg-white p-2">
-                <img src={logoUrl} alt={id} width="36px" height="36px" />
-              </div>
-              <div className="mt-2.5 kyc-box-header">
-              {providerName}
+          <div className="text-md text-justify">
+            <div className="kyc-box-icon w-[52px] rounded-md bg-white p-2">
+              <img src={logoUrl} alt={id} width="36px" height="36px" />
             </div>
+            <div>
+              <h3 className="kyc-box-header mt-2.5">{providerName}</h3>
             </div>
-
-          </>
-        </div>
+          </div>
+        </>
       </div>
     </>
   );
