1bffaa3a41c12f1130c855773233d2b1a86476cc
ckm007 <chandrakeshavmishra@gmail.com>
2024-09-24 17:32:14 +0530

    [MOSIP-35816] added deployment scripts for signup service and ui
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

---
+++ deploy/keycloak/keycloak-init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Installs all esignet keycloak-init
+## Usage: ./keycloak-init.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=signup
+CHART_VERSION=0.0.1-develop
+COPY_UTIL=../copy_cm_func.sh
+
+helm repo add mosip https://mosip.github.io/mosip-helm
+helm repo update
+
+echo "checking if mosip-pms-client, mosip-ida-client & mpartner_default_auth client is created already"
+IAMHOST_URL=$(kubectl -n esignet get cm esignet-global -o jsonpath={.data.mosip-iam-external-host})
+SIGNUP_CLIENT_SECRET_KEY='mosip_signup_client_secret'
+SIGNUP_CLIENT_SECRET_VALUE=$(kubectl -n keycloak get secrets keycloak-client-secrets -o jsonpath={.data.$SIGNUP_CLIENT_SECRET_KEY} | base64 -d)
+echo "Copying keycloak configmaps and secret"
+$COPY_UTIL configmap keycloak-host keycloak $NS
+$COPY_UTIL configmap keycloak-env-vars keycloak $NS
+$COPY_UTIL secret keycloak keycloak $NS
+
+echo "creating and adding roles to keycloak pms & mpartner_default_auth clients for ESIGNET"
+kubectl -n $NS delete secret --ignore-not-found=true keycloak-client-secrets
+helm -n $NS delete signup-keycloak-init
+helm -n $NS install signup-keycloak-init mosip/keycloak-init \
+  -f keycloak-init-values.yaml \
+  --set clientSecrets[0].name="$SIGNUP_CLIENT_SECRET_KEY" \
+  --set clientSecrets[0].secret="$SIGNUP_CLIENT_SECRET_VALUE" \
+  --version $CHART_VERSION --wait --wait-for-jobs
+
+SIGNUP_CLIENT_SECRET_VALUE=$(kubectl -n $NS get secrets keycloak-client-secrets -o jsonpath={.data.$SIGNUP_CLIENT_SECRET_KEY})
+# Check if the secret exists
+if kubectl get secret keycloak-client-secrets -n keycloak >/dev/null 2>&1; then
+  echo "Secret 'keycloak-client-secrets' exists. Performing secret update..."
+  kubectl -n keycloak get secret keycloak-client-secrets -o json |
+  jq ".data[\"$SIGNUP_CLIENT_SECRET_KEY\"]=\"$SIGNUP_CLIENT_SECRET_VALUE\"" |
+  kubectl apply -f -
+else
+  echo "Secret 'keycloak-client-secrets' does not exist. Copying the secret to the keycloak namespace."
+  $COPY_UTIL secret keycloak-client-secrets $NS keycloak
+fi
