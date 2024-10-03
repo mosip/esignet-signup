bd28a7a47be19d5704fe451b559ba3a04cc46e05
ase-101 <sunkadaeanusha@gmail.com>
2024-10-03 11:17:24 +0530

    ES-842 slot availablility and concurrent update of slot count issue fixed  (#375)
    * reusing the halted transaction cache, updated readme
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * reusing the halted transaction cache, updated readme
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * slot availablility and concurrent update of slot count issue fixed
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    ---------
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- signup-service/src/test/java/io/mosip/signup/services/IdentityVerificationServiceTest.java
+++ signup-service/src/test/java/io/mosip/signup/services/IdentityVerificationServiceTest.java
@@ -89,6 +89,7 @@ public class IdentityVerificationServiceTest {
         ReflectionTestUtils.setField(identityVerificationService, "oauthRedirectUri", "https://signup.dev.mosip.net/identity-verification");
         ReflectionTestUtils.setField(identityVerificationService, "oauthTokenUri", mockServerUri);
         ReflectionTestUtils.setField(identityVerificationService, "objectMapper", objectMapper);
+        ReflectionTestUtils.setField(identityVerificationService, "slotExpireInSeconds", 180);
         KeyPair keyPair = generateRSAKeyPair();
         X509Certificate certificate = generateSelfSignedCertificate(keyPair);
         createAndStoreKeyInP12(keyPair, certificate);
@@ -285,7 +286,7 @@ public class IdentityVerificationServiceTest {
 
         Mockito.when(cacheUtilService.getIdentityVerifierDetails()).thenReturn(identityVerifierDetails);
         String field = transactionId.concat(VALUE_SEPARATOR).concat("testSlotId");
-        Mockito.when(cacheUtilService.getSetSlotCount(field, 100)).thenReturn(10L);
+        Mockito.when(cacheUtilService.getSetSlotCount(Mockito.anyString(), Mockito.anyLong(), Mockito.anyInt())).thenReturn(10L);
         Mockito.when(cacheUtilService.setSlotAllottedTransaction(Mockito.anyString(), Mockito.any())).thenReturn(
                 identityVerificationTransaction); // Assuming maxSlotPoolSize > 10
         HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
@@ -351,7 +352,7 @@ public class IdentityVerificationServiceTest {
 
         Mockito.when(cacheUtilService.getIdentityVerifierDetails()).thenReturn(identityVerifierDetails);
         String field = "transactionId###testSlotId";
-        Mockito.when(cacheUtilService.getSetSlotCount(field, 100)).thenReturn(-1L);
+        Mockito.when(cacheUtilService.getSetSlotCount(Mockito.anyString(), Mockito.anyLong(), Mockito.anyInt())).thenReturn(-1L);
         // Execute
         try{
             identityVerificationService.getSlot("transactionId", slotRequest, null);
