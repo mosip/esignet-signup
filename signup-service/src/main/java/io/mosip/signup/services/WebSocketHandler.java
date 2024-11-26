e8d4c2ceb8d2b6435fafec91b8241f9626358826
pvsaidurga <132046494+pvsaidurga@users.noreply.github.com>
2024-11-26 21:00:18 +0530

    [ES-1923] [ES-1924] Updated web socket handler (#430)
    * [ES-1923] [ES-1924] Updated web socket handler
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * [ES-1923] Updated review comments
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * [ES-1924] Updated review comments
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * [ES-1924] Updated the review comments
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * [ES-1924] Updated unit test cases to verify simpMessagingTemplate
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * [ES-1924] Updated review comments
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    ---------
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/services/WebSocketHandler.java
+++ signup-service/src/main/java/io/mosip/signup/services/WebSocketHandler.java
@@ -13,12 +13,14 @@ import io.mosip.signup.api.exception.IdentityVerifierException;
 import io.mosip.signup.api.exception.ProfileException;
 import io.mosip.signup.api.spi.IdentityVerifierPlugin;
 import io.mosip.signup.api.spi.ProfileRegistryPlugin;
+import io.mosip.signup.api.util.ProcessFeedbackType;
 import io.mosip.signup.api.util.VerificationStatus;
 import io.mosip.signup.dto.IdentityVerificationRequest;
 import io.mosip.signup.dto.IdentityVerificationTransaction;
 import io.mosip.signup.dto.IdentityVerifierDetail;
 import io.mosip.signup.exception.InvalidTransactionException;
 import io.mosip.signup.exception.SignUpException;
+import io.mosip.signup.util.ErrorConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,10 +28,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static io.mosip.signup.api.util.ErrorConstants.IDENTITY_VERIFICATION_FAILED;
 import static io.mosip.signup.api.util.ErrorConstants.PLUGIN_NOT_FOUND;
@@ -60,25 +59,40 @@ public class WebSocketHandler {
 
 
     public void processFrames(IdentityVerificationRequest identityVerificationRequest) {
-        IdentityVerificationTransaction transaction = cacheUtilService.getVerifiedSlotTransaction(identityVerificationRequest.getSlotId());
-        if(transaction == null)
-            throw new InvalidTransactionException();
+        String errorCode = null;
+        try {
+            validate(identityVerificationRequest);
+            IdentityVerificationTransaction transaction = cacheUtilService.getVerifiedSlotTransaction(identityVerificationRequest.getSlotId());
+            if(transaction == null) {
+                log.error("Ignoring identity verification request received for unknown/expired transaction!");
+                throw new InvalidTransactionException();
+            }
 
-        IdentityVerifierPlugin plugin = identityVerifierFactory.getIdentityVerifier(transaction.getVerifierId());
-        if(plugin == null)
-            throw new SignUpException(PLUGIN_NOT_FOUND);
+            IdentityVerifierPlugin plugin = identityVerifierFactory.getIdentityVerifier(transaction.getVerifierId());
+            if(plugin == null) {
+                log.error("Ignoring identity verification request received for unknown {} IDV plugin!", identityVerificationRequest.getSlotId());
+                throw new SignUpException(PLUGIN_NOT_FOUND);
+            }
 
-        if(plugin.isStartStep(identityVerificationRequest.getStepCode())) {
-            IdentityVerificationInitDto identityVerificationInitDto = new IdentityVerificationInitDto();
-            identityVerificationInitDto.setIndividualId(transaction.getIndividualId());
-            identityVerificationInitDto.setDisabilityType(transaction.getDisabilityType());
-            plugin.initialize(identityVerificationRequest.getSlotId(), identityVerificationInitDto);
+            if(plugin.isStartStep(identityVerificationRequest.getStepCode())) {
+                IdentityVerificationInitDto identityVerificationInitDto = new IdentityVerificationInitDto();
+                identityVerificationInitDto.setIndividualId(transaction.getIndividualId());
+                identityVerificationInitDto.setDisabilityType(transaction.getDisabilityType());
+                plugin.initialize(identityVerificationRequest.getSlotId(), identityVerificationInitDto);
+            }
+
+            IdentityVerificationDto dto = new IdentityVerificationDto();
+            dto.setStepCode(identityVerificationRequest.getStepCode());
+            dto.setFrames(identityVerificationRequest.getFrames());
+            plugin.verify(identityVerificationRequest.getSlotId(), dto);
+        } catch (SignUpException e) {
+            errorCode = e.getErrorCode();
+            log.error("An error occurred while processing frames", e);
+        } finally {
+            if (errorCode != null) {
+                sendErrorFeedback(identityVerificationRequest.getSlotId(), errorCode);
+            }
         }
-
-        IdentityVerificationDto dto = new IdentityVerificationDto();
-        dto.setStepCode(identityVerificationRequest.getStepCode());
-        dto.setFrames(identityVerificationRequest.getFrames());
-        plugin.verify(identityVerificationRequest.getSlotId(), dto);
     }
 
     public void processVerificationResult(IdentityVerificationResult identityVerificationResult) {
@@ -172,5 +186,34 @@ public class WebSocketHandler {
     private long getVerificationProcessExpireTimeInMillis(IdentityVerifierDetail identityVerifierDetail) {
         int processDurationInSeconds = identityVerifierDetail.getProcessDuration() <= 0 ? slotExpireInSeconds : identityVerifierDetail.getProcessDuration();
         return System.currentTimeMillis() + ( processDurationInSeconds * 1000L );
+    }
+
+    private void sendErrorFeedback(String slotId, String errorCode) {
+        IDVProcessFeedback idvProcessFeedback = new IDVProcessFeedback();
+        idvProcessFeedback.setType(ProcessFeedbackType.ERROR);
+        idvProcessFeedback.setCode(errorCode);
+
+        IdentityVerificationResult identityVerificationResult = new IdentityVerificationResult();
+        identityVerificationResult.setFeedback(idvProcessFeedback);
+
+        simpMessagingTemplate.convertAndSend("/topic/" + slotId, identityVerificationResult);
+    }
+
+    private void validate(IdentityVerificationRequest request) {
+        if (request.getStepCode() == null || request.getStepCode().isBlank()) {
+            throw new SignUpException(ErrorConstants.INVALID_STEP_CODE);
+        }
+
+        List<FrameDetail> frames = request.getFrames();
+        if (frames != null && !frames.isEmpty()) {
+            for (FrameDetail frame : frames) {
+                if (frame.getFrame() == null || frame.getFrame().isBlank()) {
+                    throw new SignUpException(ErrorConstants.INVALID_FRAME);
+                }
+                if (frame.getOrder() < 0) {
+                    throw new SignUpException(ErrorConstants.INVALID_ORDER);
+                }
+            }
+        }
     }
 }
