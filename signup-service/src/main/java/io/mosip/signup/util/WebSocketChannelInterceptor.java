fb3ce1c96af21cf382edcbdaa10f7e5f70c86603
Sachin Rana <sacrana324@gmail.com>
2025-01-06 02:21:37 +0530

    updated method name and exception class
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/util/WebSocketChannelInterceptor.java
+++ signup-service/src/main/java/io/mosip/signup/util/WebSocketChannelInterceptor.java
@@ -1,10 +1,10 @@
 package io.mosip.signup.util;
 
-import io.mosip.signup.exception.SignUpException;
 import io.mosip.signup.services.CacheUtilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageDeliveryException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -20,17 +20,17 @@ public class WebSocketChannelInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        if(isTopicSubscription(accessor)) {
+        if(isSlotSubscriptionMessage(accessor)) {
             String destination = accessor.getDestination();
             String slotId = destination.replace("/topic/", "");
             if(cacheUtilService.getVerifiedSlotTransaction(slotId) == null) {
-                throw new SignUpException(ErrorConstants.INVALID_SLOT_ID);
+                throw new MessageDeliveryException(ErrorConstants.INVALID_SLOT_ID);
             }
         }
         return message;
     }
 
-    private boolean isTopicSubscription(StompHeaderAccessor accessor) {
+    private boolean isSlotSubscriptionMessage(StompHeaderAccessor accessor) {
         return accessor != null
                 && StompCommand.SUBSCRIBE.equals(accessor.getCommand())
                 && accessor.getDestination() != null
