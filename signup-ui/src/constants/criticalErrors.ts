365b3f1284a57dfeb2350a6606c6621fc59710fa
Gk <76690271+gk-4VII@users.noreply.github.com>
2025-01-15 16:55:36 +0530

    merge of all 1.5.1 related bug fixes. (#519)
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

---
+++ signup-ui/src/constants/criticalErrors.ts
@@ -0,0 +1,14 @@
+export const criticalErrorsToPopup = [
+    "invalid_transaction",
+    "identifier_not_found",
+    "kbi_challenge_not_found",
+    "identifier_mismatch",
+    "identifier_already_registered",
+];
+
+export const criticalErrorsWithStaticDesc = [
+    "identifier_not_found",
+    "kbi_challenge_not_found",
+    "identifier_mismatch",
+    "identifier_already_registered",
+];
