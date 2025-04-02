e8c89efd8e4815e7238d1f1a9f5da85f19680c13
ase-101 <sunkadaeanusha@gmail.com>
2025-04-02 20:16:52 +0530

    ES-2135 (#618)
    * ES-2135
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    * Tracing support added
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    ---------
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/SignUpServiceApplication.java
+++ signup-service/src/main/java/io/mosip/signup/SignUpServiceApplication.java
@@ -5,23 +5,30 @@
  */
 package io.mosip.signup;
 
-import io.mosip.esignet.core.config.RedisCacheConfig;
-import io.mosip.esignet.core.config.SharedComponentConfig;
-import io.mosip.esignet.core.config.SimpleCacheConfig;
+import brave.Tracer;
+import io.mosip.esignet.core.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableCaching
 @EnableAsync
-@SpringBootApplication(scanBasePackages = "io.mosip.signup.*,io.mosip.kernel.auth.defaultadapter," +
+@SpringBootApplication(scanBasePackages = "io.mosip.signup.*," +
+        "io.mosip.kernel.auth.defaultadapter," +
         "${mosip.signup.integration.impl.basepackage}")
-@Import({SharedComponentConfig.class, RedisCacheConfig.class, SimpleCacheConfig.class})
+@Import({SharedComponentConfig.class, RedisCacheConfig.class, SimpleCacheConfig.class,
+        AccessLogSleuthConfiguration.class, TraceAutoConfiguration.class})
 public class SignUpServiceApplication {
+
+    @Bean
+    public SleuthValve sleuthValve(Tracer tracer) {
+        return new SleuthValve(tracer);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SignUpServiceApplication.class, args);
