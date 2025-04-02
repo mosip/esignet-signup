4d61c230bbb2905f488118b8fcb03f8925fca48a
ase-101 <sunkadaeanusha@gmail.com>
2025-04-02 08:26:59 +0530

    ES-2135
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/SignUpServiceApplication.java
+++ signup-service/src/main/java/io/mosip/signup/SignUpServiceApplication.java
@@ -5,12 +5,13 @@
  */
 package io.mosip.signup;
 
-import io.mosip.esignet.core.config.RedisCacheConfig;
-import io.mosip.esignet.core.config.SharedComponentConfig;
-import io.mosip.esignet.core.config.SimpleCacheConfig;
+import io.mosip.esignet.core.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,9 +19,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableCaching
 @EnableAsync
-@SpringBootApplication(scanBasePackages = "io.mosip.signup.*,io.mosip.kernel.auth.defaultadapter," +
-        "${mosip.signup.integration.impl.basepackage}")
-@Import({SharedComponentConfig.class, RedisCacheConfig.class, SimpleCacheConfig.class})
+@SpringBootApplication
+@ComponentScans({
+        @ComponentScan(basePackages = "io.mosip.signup.*"),
+        @ComponentScan(basePackages = "io.mosip.kernel.auth.defaultadapter"),
+        @ComponentScan(basePackages = "${mosip.signup.integration.impl.basepackage}"),
+        @ComponentScan(basePackages = "io.mosip.esignet.core.config", includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SleuthValve.class))
+})
+/*@SpringBootApplication(scanBasePackages = "io.mosip.signup.*," +
+        "io.mosip.kernel.auth.defaultadapter," +
+        "io.mosip.esignet.core.config.SleuthValve," +
+        "${mosip.signup.integration.impl.basepackage}", )*/
+@Import({SharedComponentConfig.class, RedisCacheConfig.class, SimpleCacheConfig.class, AccessLogSleuthConfiguration.class})
 public class SignUpServiceApplication {
 
     public static void main(String[] args) {
