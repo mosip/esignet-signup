1fc244c55510f79c94c07b21de7bb2bb345c7b04
ase-101 <sunkadaeanusha@gmail.com>
2025-07-21 13:34:01 +0530

    cherry pick PR 694 from ES-2409 to dev
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>

--- signup-service/src/main/java/io/mosip/signup/config/WebSocketConfig.java
+++ signup-service/src/main/java/io/mosip/signup/config/WebSocketConfig.java
@@ -5,9 +5,10 @@
  */
 package io.mosip.signup.config;
 
-import io.mosip.signup.services.WebSocketHandshakeHandler;
-import io.mosip.signup.util.WebSocketChannelInterceptor;
+import io.mosip.signup.ws.WebSocketHandshakeHandler;
+import io.mosip.signup.ws.WebSocketChannelInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -25,6 +26,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     private WebSocketChannelInterceptor webSocketChannelInterceptor;
 
+    @Value("${mosip.signup.ws.inbound.message.size.mb:3}")
+    private int inboundMessageSizeInMB;
+
+    @Value("${mosip.signup.ws.outbound.message.size.mb:1}")
+    private int outboundMessageSizeInMB;
+
+    @Value("${mosip.signup.ws.allowed.origin:*}")
+    private String allowedOrigin;
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(webSocketChannelInterceptor);
@@ -39,7 +49,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //By default, only same origin requests are allowed, should take the origin from properties
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins(allowedOrigin)
                 .setHandshakeHandler(webSocketHandshakeHandler);
+    }
+
+    @Override
+    public void configureWebSocketTransport(org.springframework.web.socket.config.annotation.WebSocketTransportRegistration registration) {
+        // Set the maximum size for incoming text messages (decoded)
+        // Default is often 64KB. You'll need to increase this significantly.
+        // 1 MB = 1024 * 1024 bytes
+        registration.setMessageSizeLimit(inboundMessageSizeInMB * 1024 * 1024);
+        registration.setSendBufferSizeLimit(outboundMessageSizeInMB * 1024 * 1024);
+        //registration.setSendTimeLimit(20 * 1000);
     }
 }
