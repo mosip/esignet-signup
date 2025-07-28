7dc7b88885a437fbf8362522b4af249ac3a5ee2b
Praful Rakhade <prafulrakhade02@gmail.com>
2025-07-28 15:35:55 +0530

    [DSD-8608] eSignet 1.6.1 post release changes
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/signup-service/install.sh
+++ deploy/signup-service/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=signup
-CHART_VERSION=1.2.1
+CHART_VERSION=1.2.1-develop
 
 while true; do
     read -p "Do you want to continue installing signup services? (y/n): " ans
@@ -34,7 +34,7 @@ function installing_signup() {
   done
 
   NS=signup
-  CHART_VERSION=1.2.1
+  CHART_VERSION=1.2.1-develop
 
   helm repo add mosip https://mosip.github.io/mosip-helm
   helm repo update
