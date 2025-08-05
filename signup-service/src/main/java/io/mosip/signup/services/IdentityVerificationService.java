9090e97959e14704c7f85998a95148b2a244900c
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2025-08-05 09:42:37 +0530

    [MOSIP-42502] http header splitting resource
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- signup-service/src/main/java/io/mosip/signup/services/IdentityVerificationService.java
+++ signup-service/src/main/java/io/mosip/signup/services/IdentityVerificationService.java
@@ -343,7 +343,7 @@ public class IdentityVerificationService {
         unsetCookie.setPath("/");
         response.addCookie(unsetCookie);
 
-        Cookie cookie = new Cookie(SignUpConstants.IDV_SLOT_ALLOTTED, value);
+        Cookie cookie = new Cookie(SignUpConstants.IDV_SLOT_ALLOTTED, value.replaceAll("[^a-zA-Z]", ""));
         int maxAge = identityVerifierDetail.getProcessDuration() > 0 ? identityVerifierDetail.getProcessDuration() : verifiedSlotTimeout;
         cookie.setMaxAge(slotAllottedTimeout+maxAge);
         cookie.setHttpOnly(true);
