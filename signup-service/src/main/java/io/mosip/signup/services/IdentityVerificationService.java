dd35ab017633b415ca70e82e50d6e1342520f07d
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2025-08-07 18:04:48 +0530

    [DSD-8679] issue resolved while setting cookie it remove special character
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- signup-service/src/main/java/io/mosip/signup/services/IdentityVerificationService.java
+++ signup-service/src/main/java/io/mosip/signup/services/IdentityVerificationService.java
@@ -195,7 +195,7 @@ public class IdentityVerificationService {
                 .findFirst();
 
         if(result.isPresent()) {
-           return getIdentityVerifierMetadata(identityVerifierId);
+           return getIdentityVerifierMetadata(identityVerifierId.replaceAll("[^a-zA-Z0-9+_#-]", ""));
         }
         log.error("Invalid identity verifier ID provided!");
         throw new SignUpException(ErrorConstants.INVALID_IDENTITY_VERIFIER_ID);
@@ -343,7 +343,7 @@ public class IdentityVerificationService {
         unsetCookie.setPath("/");
         response.addCookie(unsetCookie);
 
-        Cookie cookie = new Cookie(SignUpConstants.IDV_SLOT_ALLOTTED, value);
+        Cookie cookie = new Cookie(SignUpConstants.IDV_SLOT_ALLOTTED, value.replaceAll("[^a-zA-Z0-9+_#-]", ""));
         int maxAge = identityVerifierDetail.getProcessDuration() > 0 ? identityVerifierDetail.getProcessDuration() : verifiedSlotTimeout;
         cookie.setMaxAge(slotAllottedTimeout+maxAge);
         cookie.setHttpOnly(true);
