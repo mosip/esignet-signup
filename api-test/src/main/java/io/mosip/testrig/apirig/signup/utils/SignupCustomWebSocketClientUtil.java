557d3d7e635268b8d992807da95fc79f9a184fdd
Nandhukumar <nandhukumare@gmail.com>
2025-08-26 23:02:44 +0530

    websocket fixes
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/signup/utils/SignupCustomWebSocketClientUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/signup/utils/SignupCustomWebSocketClientUtil.java
@@ -20,20 +20,4 @@ public class SignupCustomWebSocketClientUtil extends WebSocketClientUtil {
     	logger.info("Custom close reason: " + closeReason);
 
     }
-    
- // Explicit close method for your testcases
-//    public void closeWithNormalClosure() {
-//        try {
-//            if (getSession() != null && getSession().isOpen()) {
-//                CloseReason reason = new CloseReason(
-//                        CloseReason.CloseCodes.NORMAL_CLOSURE,
-//                        "Shutting down cleanly"
-//                );
-//                getSession().close(reason);
-//                logger.info("Closed WebSocket with Normal Closure");
-//            }
-//        } catch (Exception e) {
-//            logger.error("Error closing WebSocket", e);
-//        }
-//    }
 }
