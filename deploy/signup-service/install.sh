64577efc34989b903802effa85a6eea8247aa78b
Prafulrakhade <prafulrakhade02@gmail.com>
2025-12-04 19:18:06 +0530

    [DSD-9388] eSignet 1.7.0 release
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/signup-service/install.sh
+++ deploy/signup-service/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=signup
-CHART_VERSION=1.3.0
+CHART_VERSION=1.3.0-develop
 
 while true; do
     read -p "Do you want to continue installing signup services? (y/n): " ans
