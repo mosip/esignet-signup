e4e78e52a19cffe1aef35b7e8686858ff6b1ba1a
Abhi <abhishek.shankarcs@gmail.com>
2025-12-17 12:30:19 +0530

    [MOSIP-44077]Updated the develop branch installation scripts with the release-1.3.x changes
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- deploy/signup-service/install.sh
+++ deploy/signup-service/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=signup
-CHART_VERSION=1.2.2-develop
+CHART_VERSION=0.0.1-develop
 
 while true; do
     read -p "Do you want to continue installing signup services? (y/n): " ans
