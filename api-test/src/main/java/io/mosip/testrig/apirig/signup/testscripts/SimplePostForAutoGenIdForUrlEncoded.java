251e3ac2e7c89e1ba70a1cba1182b61c46898417
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-12-17 17:30:11 +0530

    Resolved Comments
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/signup/testscripts/SimplePostForAutoGenIdForUrlEncoded.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/signup/testscripts/SimplePostForAutoGenIdForUrlEncoded.java
@@ -146,7 +146,7 @@ public class SimplePostForAutoGenIdForUrlEncoded extends SignupUtil implements I
 				String tempUrl = SignupConfigManager.getEsignetBaseUrl();
 				String endPoint = tempUrl + testCaseDTO.getEndPoint();
 				if (testCaseDTO.getEndPoint().contains("$GETENDPOINTFROMWELLKNOWN$")
-						&& BaseTestCase.currentModule.contains("signup")) {
+						&& BaseTestCase.currentModule.toLowerCase().contains("signup")) {
 					endPoint = SignupUtil.getValueFromEsignetWellKnownEndPoint("token_endpoint",
 							SignupConfigManager.getEsignetBaseUrl());
 				}
