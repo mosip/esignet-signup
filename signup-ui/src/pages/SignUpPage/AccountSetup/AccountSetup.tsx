72818ebd07bcb27be9d633c8023a9f8ef505e5b5
Sajid Mannikeri <sajid.mannikeri@ad.infosys.com>
2026-01-14 12:57:24 +0530

    update handleRedirect method
    Signed-off-by: Sajid Mannikeri <sajid.mannikeri@ad.infosys.com>

--- signup-ui/src/pages/SignUpPage/AccountSetup/AccountSetup.tsx
+++ signup-ui/src/pages/SignUpPage/AccountSetup/AccountSetup.tsx
@@ -134,6 +134,7 @@ export const AccountSetup = ({ settings, methods }: AccountSetupProps) => {
         consent: data.consent ? "AGREE" : "DISAGREE",
         userInfo: {
           ...data,
+          'IDSchemaVersion': '0.1',
         },
         locale: i18n.language,
       },
