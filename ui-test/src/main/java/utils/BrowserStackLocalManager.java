cc238c22ee61d1d7cc410f2904a3fa7db0d0316a
Bhuvanashree B S <bhuvanashree.b@cyberpwn.com>
2026-02-10 16:52:33 +0530

    Fix BrowserStack Local tunnel issues and stabilize parallel execution
    Signed-off-by: Bhuvanashree B S <bhuvanashree.b@cyberpwn.com>

--- ui-test/src/main/java/utils/BrowserStackLocalManager.java
+++ ui-test/src/main/java/utils/BrowserStackLocalManager.java
@@ -6,12 +6,19 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 
 import com.browserstack.local.Local;
+import org.apache.log4j.Logger;
 
 public class BrowserStackLocalManager {
 
+	private static final Logger LOGGER = Logger.getLogger(BrowserStackLocalManager.class);
 	private static Local bsLocal;
 
 	public synchronized static void start() throws Exception {
+
+		if (bsLocal != null && bsLocal.isRunning()) {
+			return;
+		}
+
 		bsLocal = new Local();
 		String accessKey = StringUtils.isBlank(EsignetConfigManager.getproperty("browserstack_access_key"))
 				? BaseTestUtil.getKeyValueFromYaml("/browserstack.yml", "accessKey")
@@ -20,16 +27,29 @@ public class BrowserStackLocalManager {
 		Map<String, String> args = new HashMap<>();
 		args.put("key", accessKey);
 		args.put("forceLocal", "true");
-		args.put("onlyAutomate", "true");
+		args.put("verbose", "true");
+		args.put("logFile", "bs_local.log");
 
 		bsLocal.start(args);
-		System.out.println("BrowserStack Local started");
+
+		// Wait till tunnel is fully running
+		int retries = 0;
+		while (!bsLocal.isRunning() && retries < 15) {
+			Thread.sleep(1000);
+			retries++;
+		}
+
+		if (!bsLocal.isRunning()) {
+			throw new RuntimeException("BrowserStack Local failed to start");
+		}
+
+		LOGGER.info("BrowserStack Local started successfully");
 	}
 
 	public synchronized static void stop() throws Exception {
-		if (bsLocal != null) {
+		if (bsLocal != null && bsLocal.isRunning()) {
 			bsLocal.stop();
-			System.out.println("BrowserStack Local stopped");
+			LOGGER.info("BrowserStack Local stopped");
 		}
 	}
 }
